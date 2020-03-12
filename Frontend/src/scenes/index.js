16c80c43d59fdc8d32ed8d0df7224ec9d4a5f238
Josh <Joshua-Howard@users.noreply.github.com>
2020-03-11 20:01:12 -0400

    Initial Pie Chart with Comments

--- Frontend/src/scenes/index.js
+++ Frontend/src/scenes/index.js
@@ -1,24 +1,55 @@
 import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import * as d3 from 'd3';
 import './styles.sass';
+
+import AnimatedPie from './animatedPie';
 
 // import * as actions from '../redux/actions/actions';
 // const mapStateToProps = state => ({});
 // const mapDispatchToProps = dispatch => ({});
 
-const Scenes = ({}) => {
-  const [count, setCount] = useState(0);
+const TestData = [
+  { city: 0, value: 17 },
+  { city: 1, value: 19 },
+  { city: 2, value: 1 },
+  { city: 3, value: 4 },
+  { city: 4, value: 98 }
+];
+
+const TestData2 = [
+  { city: 0, value: 30 },
+  { city: 1, value: 50 },
+  { city: 2, value: 70 },
+  { city: 3, value: 40 },
+  { city: 4, value: 8 }
+];
+
+const PieChart = ({}) => {
+  const [sourceData, updateSourceData] = useState(false);
 
   return (
     <div>
-      <div>Count {count}</div>
+      <div>SVG Below</div>
 
-      <button onClick={() => setCount(lastCount => lastCount + 1)}>
-        Click Here to Increase the Count
+      <div>{JSON.stringify(sourceData ? TestData : TestData2)}</div>
+
+      <button
+        className="w-100 my-3"
+        onClick={() => updateSourceData(last => !last)}
+      >
+        Swap Data
       </button>
+
+      <AnimatedPie
+        data={sourceData ? TestData : TestData2}
+        width={200}
+        height={200}
+        innerRadius={60}
+        outerRadius={100}
+      />
     </div>
   );
 };
 
-export default Scenes;
+export default PieChart;
 // export default connect(mapStateToProps, mapDispatchToProps)(Scenes);
